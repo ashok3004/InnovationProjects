8fb570e7b841395929502a2919bc84c887c7eeb9
ashok3004 <32065442+ashok3004@users.noreply.github.com>
2023-02-11 13:25:34 +0530

    Update views.py

--- ams/Firstapp/views.py
+++ ams/Firstapp/views.py
@@ -10,4 +10,5 @@ def add(request):
     val1=int(request.POST['num1'])
     val2=int(request.POST['num2'])
     res=val1+val2
+    res=val1-val2
     return render(request, "result.html", {'result':res})
